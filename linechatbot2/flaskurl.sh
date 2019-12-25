0715eca50e215895794bfa6bbd918374c19156a2
kao <j3644742@gmail.com>
2019-12-25 20:15:35 +0800

    1225

--- linechatbot2/flaskurl.sh
+++ linechatbot2/flaskurl.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-curl -s "$(docker port flask_ngrok 4040)/api/tunnels" | awk -F',' '{print $3}' | awk -F'"' '{print $4}' | awk -F'//' '{print $2}'
+curl -s "localhost:54088/api/tunnels" | awk -F',' '{print $3}' | awk -F'"' '{print $4}' | awk -F'//' '{print $2}'
